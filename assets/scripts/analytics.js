c25bdd7c2457efe464128c6ecdda12a4fc296603
mohamed96321 <atefm.me499@gmail.com>
2023-11-15 14:16:33 +0200

    Initial project

---
+++ assets/scripts/analytics.js
@@ -0,0 +1,11 @@
+const intervalId = setInterval(() => {
+  console.log('Sending analytics data...')
+  }, 2000
+);
+
+document.getElementById('stop-analytics-btn').addEventListener(
+  'click', 
+  () => {
+    clearInterval(intervalId);
+  }
+);
---
+++ assets/scripts/events.js
@@ -0,0 +1,18 @@
+const button = document.querySelector('button');
+
+// button.onclick = function() {};
+
+// const buttonClickHandle = () => {
+//   alert('Button was clicked!');
+// };
+
+// const anotherButtonClickHandler = () => {
+//   buttonClickHandle();
+//   console.log('This was clicked!');
+// };
+
+// button.onclick = anotherButtonClickHandler;
+
+button.addEventListener();
+
+button.removeEventListener();
